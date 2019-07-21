03b4ce4c8b9d201e86827bddbdd9ae236946547a
Jan Varmuža <jan.varmuza@gmail.com>
2019-07-22 00:09:46 +0200

    Updates

---
+++ precache-manifest.4760d355b0573e89000c47a5dee42965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f46c9b1b715f03446b43214e6244a2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b4d2ddc8ffe5bcd677a",
+    "url": "/monsters-rolodex/static/css/main.733fe0a0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5b4d2ddc8ffe5bcd677a",
+    "url": "/monsters-rolodex/static/js/main.991ae2c5.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
